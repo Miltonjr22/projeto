b7d1600a3876ab3da792e7dc25bf9c443286ba84
Miltonjr22 <144274875+Miltonjr22@users.noreply.github.com>
2024-03-10 17:54:50 -0300

    Add files via upload

---
+++ um.js
@@ -0,0 +1,17 @@
+function funcaoTradicionalSemParametro() {
+  console.log("Função tradicional sem parâmetros.");
+}
+
+function soma(a, b) {
+  return a + b;
+}
+
+const multiplicacao = (x, y) => {
+  return x * y;
+};
+
+funcaoTradicionalSemParametro();
+
+console.log(soma(5, 3));
+
+console.log(multiplicacao(4, 6));
